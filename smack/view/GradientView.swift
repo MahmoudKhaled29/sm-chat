2a76690a2aa765ab4085e4d8353bea17372b2413
Mahmoud Khaled <mahmoudkhaled@Mahmouds-Mac.local>
2018-06-20 20:30:25 +0200

    editing reear view

---
+++ smack/view/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  smack
+//
+//  Created by Mahmoud Khaled on 6/20/18.
+//  Copyright © 2018 Mahmoud Khaled. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
